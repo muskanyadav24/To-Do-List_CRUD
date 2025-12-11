4ebec98590fbb6f950c0197029afed5db1e17aca
Muskan yadav <muskanyadav92163@gmail.com>
2025-12-11 20:26:20 +0530

    first commit

---
+++ models/todoModel.js
@@ -0,0 +1,23 @@
+let todos = [];
+
+module.exports = {
+    getAllTodos(){
+        return todos;
+    },
+
+    addTodo(todo){
+        todos.push(todo);
+    },
+
+    getTodoById(id){
+        return todos[id];
+    },
+
+    updateTodo(id, updatedTodo){
+        todos[id] = updatedTodo;
+    },
+
+    deleteTodo(id){
+        todos.splice(id, 1);
+    }
+}
