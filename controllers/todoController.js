574feafabd3513c06ed39f6306666efbadb12e5c
Muskan yadav <muskanyadav92163@gmail.com>
2026-02-08 08:51:33 +0530

    Refactor todoController to use local function syntax

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,23 +1,23 @@
 const todoModel = require('../models/todoModel');
 
 // home page
-exports.home = (req, res) => {
+const home = (req, res) => {
     res.render('index');
 };
 
 // add todo page
-exports.addTodoPage = (req, res) => {
+const addTodoPage = (req, res) => {
     res.render('./pages/addTodo');
 };
 
 // view todos page
-exports.viewTodos = (req, res) => {
+const viewTodos = (req, res) => {
     const todos = todoModel.getAllTodos();
     res.render('./pages/viewTodo', { todos });
 };
 
 // add todo process
-exports.addTodo = (req, res) => {
+const addTodo = (req, res) => {
     const todo = {
         name: req.body.todos,
         description: 'pending',
@@ -29,14 +29,14 @@ exports.addTodo = (req, res) => {
 };
 
 // edit todo page
-exports.editTodoPage = (req, res) => {
+const editTodoPage = (req, res) => {
     const id = req.params.id;
     const todo = todoModel.getTodoById(id);
     res.render('./pages/editTodo', { todo, id });
 };
 
 // update todo process
-exports.updateTodo = (req, res) => {
+const updateTodo = (req, res) => {
     const id = req.params.id;
     const updatedTodo = {
         name: req.body.todos,
@@ -49,13 +49,13 @@ exports.updateTodo = (req, res) => {
 };
 
 // delete todo
-exports.deleteTodo = (req, res) => {
+const deleteTodo = (req, res) => {
     const id = req.params.id;
     todoModel.deleteTodo(id);
     res.redirect('/viewTodo');
 };
 
 // service page
-exports.service = (req, res) => {
+const service = (req, res) => {
     res.render('./pages/service');
 };
